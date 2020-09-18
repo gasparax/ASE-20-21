94fb332375084bb0400a4ee90c11d012df5fe58b
Nunzio <nunziolopardo@live.it>
2020-09-18 15:20:44 +0200

    fixed code

--- Lab_1/Calculator/calculator.py
+++ Lab_1/Calculator/calculator.py
@@ -1,19 +1,31 @@
-#calculator.py
+# calculator.py
 
-def sum(m, n):
-    m = m + n
-    for i in range(0, n):
-        m += 1
-    return m
+def sum (m, n):
+    result = m
+    if (n < 0):
+        for i in range (abs(n)):
+            result = result - 1
+    else:
+        for i in range (n):
+            result = result + 1
+
+    return result
 
 
-def divide(m, n):
+# Substract n from m until it gets to 0
+def divide (m, n):
     if n == 0:
         raise ZeroDivisionError
-    m = abs(m)
-    n = abs(n)
+
     result = 0
-    while m > 0:
-        result += 1
-        m -= n
-    return m
+    negative = m > 0 and n < 0 or m < 0 and n > 0
+    n = abs (n)
+    m = abs (m)
+
+    while (m - n >= 0):
+        m = m - n
+        result = result + 1
+
+    result = -result if negative else result
+
+    return result
